b690348c4541074ff7ddc5845af1d3de34adf116
4318 Loknovskaya A.D. <a.loknovskaya@yandex.ru>
2025-12-06 18:24:38 +0300

    qdrant+minio (не работает)

--- parser/qdrnt.py
+++ parser/qdrnt.py
@@ -1,33 +1,49 @@
 import json
 from qdrant_client import QdrantClient
-from qdrant_client.conversions.common_types import VectorParams
+from qdrant_client.models import VectorParams, Distance
 
-#путь к JSONL (ПОМЕНЯТЬ)
-jsonl_file_path = r'D:\inf\TP\cursach\embeddings.jsonl'
+from rag_sources.minio_client import get_minio_client # НЕ РАБОТАЕТ
+import io
 
+# инициализация клиента Minio
+minio_client = get_minio_client()
+
+# путь к объекту в Minio (попытка хотя бы с 1 файлом из 2)
+jsonl_object_path = 'embeddings/all_chunks.jsonl'  
+bucket_name = 'rag-sources' 
+
+# клиент Qdrant
 client = QdrantClient(
     url='http://212.192.220.24:6333',
     api_key='pii5z%cE1'
 )
-#создание и проверка существования коллекции
-collection_name = 'text_embeddings'
-collections = [col.name for col in client.get_collections().collections]
 
+collection_name = 'text_embeddings'
+
+# проверка и создание коллекции
+collections = [col.name for col in client.get_collections().collections]
 if collection_name not in collections:
     print(f"Создаю коллекцию '{collection_name}'...")
     vector_size = 384
     client.create_collection(
         collection_name=collection_name,
-        vectors_config=VectorParams(size=vector_size, distance='Cosine')
+        vectors_config=VectorParams(size=vector_size, distance=Distance.COSINE)
     )
     print(f"Коллекция '{collection_name}' успешно создана.")
 else:
     print(f"Коллекция '{collection_name}' уже существует.")
 
-#чтение, подготовка и формирование точек
+# получение файла из Minio
+response = minio_client.get_object(bucket_name, jsonl_object_path)
+
 points = []
-with open(jsonl_file_path, 'r', encoding='utf-8') as f:
-    for line in f:
+
+try:
+    # читаем весь контент как байты
+    data = response.read()
+    text_data = data.decode('utf-8')
+    # парсим каждую строку
+    for line in text_data.splitlines():
         obj = json.loads(line)
         id_ = obj['id']
         text = obj['text']
@@ -45,8 +61,10 @@ with open(jsonl_file_path, 'r', encoding='utf-8') as f:
             "payload": payload
         }
         points.append(point)
+finally:
+    response.close()
 
-#деление на батчи и вставка
+# деление на батчи и загрузка
 batch_size = 1000
 total_points = len(points)
 
